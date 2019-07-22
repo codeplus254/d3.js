aa1c866fa95d550d69d10f22151270eaf0a92df6
https://github.com': codeplus254 <ronnymageh@gmail.com>
2019-07-22 14:13:56 +0300

    Webpack 4 successfully configured!!!

--- bundle.js
+++
@@ -1,8 +0,0 @@
-(function (d3) {
-  'use strict'
-  
-  const svg = d3.select('svg');
-   svg.style('background-color','red') 
-} (d3))
-       
-       
--- index.js
+++
@@ -1,8 +0,0 @@
-import { select } from 'd3';
-
-const svg =    select('svg');
-
-const circle = svg.append('circle');
-circle.attr('r',200);
-       
-    
