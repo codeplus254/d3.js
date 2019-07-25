37b373173a1567c7ec9bcfee8947bb3436611994
https://github.com': codeplus254 <ronnymageh@gmail.com>
2019-07-25 12:24:26 +0300

    created an area chart

--- src/index.js
+++ src/index.js
@@ -1,28 +1,34 @@
 import { smileAtTheCamera } from './smiley/smiley';
 import { showBarchart } from './bar_chart/barChart';
 import { showScatterPlot } from './scatterplot/scatterPlot';
-import { showLineAndAreaPlots } from './line_chart/line_chart';
+import { showLineChart } from './line_chart/line_chart';
+import { showAreaChart } from './area_chart/area_chart';
+
 import './styles/styles.css';
 
-let page = 'lineChart';
+let page = 'areaChart';
 switch(page) {
   case 'barChart':
     smileAtTheCamera(false);
     showScatterPlot(false);
-    showLineAndAreaPlots(false);
+    showLineChart(false);
     showBarchart(true);
     break;
   case 'scatterPlot':
     smileAtTheCamera(false);
     showBarchart(false);
-    showLineAndAreaPlots(false);
+    showLineChart(false);
     showScatterPlot(true);
     break;
   case 'lineChart':
     smileAtTheCamera(false);
     showBarchart(false);
     showScatterPlot(false);
-    showLineAndAreaPlots(true);
+    showLineChart(true);
+    break;
+  case 'areaChart':
+    smileAtTheCamera(false);
+    showAreaChart(true);
     break;
   default:
     showBarchart(false);
