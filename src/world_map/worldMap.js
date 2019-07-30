3f38e45376229b064f01e46e916b8b116512b818
https://github.com': codeplus254 <ronnymageh@gmail.com>
2019-07-30 16:48:52 +0300

    enabled zooming in on the map as well as country names

--- src/world_map/worldMap.js
+++ src/world_map/worldMap.js
@@ -1,4 +1,4 @@
-import { select, geoPath, geoNaturalEarth1 } from 'd3';
+import { select, json, tsv, zoom, event,  geoPath, geoNaturalEarth1 } from 'd3';
 import * as topojson from "topojson-client";
 import './worldMap.css'
 
@@ -9,19 +9,36 @@ const svg = select('svg');
 
 const projection = geoNaturalEarth1();
 const pathGenerator=geoPath().projection(projection);
-
-svg.append('path')
+const g = svg.append('g');
+g.append('path')
   .attr('class', 'sphere')
   .attr('d', pathGenerator({type: 'Sphere'}));
 
-const world = require("../../node_modules/world-atlas/world/110m.json" );
-const countries = topojson.feature(world, world.objects.countries);
 
-svg.selectAll('path')
-  .data(countries.features)
-  .enter().append('path')
-    .attr('class', 'country')
-    .attr('d', pathGenerator);
+svg.call(zoom().on('zoom', () => {
+    g.attr('transform', event.transform);
+}));
+
+Promise.all([
+  tsv('../../node_modules/world-atlas/world/50m.tsv'),
+  json('../../node_modules/world-atlas/world/50m.json')
+]).then(([tsvData, topoJSONData]) => {
+  const countryName = {};
+  tsvData.forEach(d => {
+    countryName[d.iso_n3] = d.name;
+  });
+
+    const countries = topojson.feature(topoJSONData, topoJSONData.objects.countries);
+    g.selectAll('path').data(countries.features)
+      .enter().append('path')
+        .attr('class', 'country')
+        .attr('d', pathGenerator)
+      .append('title')
+        .text(d => countryName[d.id]);
+} );
+
+
+
 
 
 const showWorld = boolean => {
